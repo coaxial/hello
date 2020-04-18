e49bdd7bc547265abc8f94ddd730e7bc7411d235
coaxial <hi@64b.it>
2020-04-18 16:57:30 +0200

    Use node-fetch instead of http

--- functions/weather.js
+++ functions/weather.js
@@ -1,64 +1,36 @@
 'use strict'
 
 const config = require('dotenv').config()
-const http = require('http')
+const fetch = require('node-fetch')
 
 function handler(event, context, callback) {
   const weatherInWords = '%C'
   const weatherIcon = '%c'
   const temperature = '%t'
   const metricUnits = 'm'
+  // wttr.in has trouble fulfilling the request if output looks like an object,
+  // so using an array instead.
   const outputFormat = `["${weatherInWords}", "${weatherIcon}", "${temperature}"]`
-  http.get(
-    `http://wttr.in/${config.WEATHER_LOCATION}?${metricUnits}&format=${outputFormat}`,
-    response => {
+  const url = `https://wttr.in/${config.WEATHER_LOCATION}?${metricUnits}&format=${outputFormat}`
+
+  fetch(url)
+    .then(response => response.json())
+    .then(json => {
       const HTTP_OK = 200
-      const HTTP_ERROR = 500
-      const { statusCode } = response
-      
-      if(statusCode !== HTTP_OK) {
-        return callback(
-          null,
-          {
-            statusCode: HTTP_ERROR,
-            body: JSON.stringify({
-              message: 'Weather API error.',
-            }),
-          },
-        )
+      const weather = {
+        words: json[0].toLowerCase(),
+        icon: json[1],
+        temp: json[2],
       }
 
-      let rawData = ''
-      response.setEncoding('utf-8')
-      response.on('data', data => rawData += data)
-      response.on('end', () => {
-        const json = JSON.parse(rawData)
-        const weather = {
-          words: json[0].toLowerCase(),
-          icon: json[1],
-          temp: json[2],
-        }
-
-        return callback(
-          null,
-          {
-            statusCode: HTTP_OK,
-            body: JSON.stringify({ weather }),
-          },
-        )
-      })
-    },
-  ).on('error', () => {
-    return callback(
-      null,
-      {
-        statusCode: HTTP_ERROR,
-        body: JSON.stringify({
-          message: 'Weather API error.',
-        }),
-      },
-    )
-  })
+      return callback(
+        null,
+        {
+          statusCode: HTTP_OK,
+          body: JSON.stringify({ weather }),
+        },
+      )
+    })
 }
 
 module.exports = { handler }
